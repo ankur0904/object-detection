e5802e7e4cb0def03342be42dd5ded879bfee2ac
ankur0904 <ankursingh91002@gmail.com>
2024-06-17 11:13:22 +0530

    Adds fine-tuned model for bottle

--- main.py
+++ main.py
@@ -8,94 +8,13 @@ import math
 # cap.set(4, 720)  # height
 
 
-cap = cv2.VideoCapture("../Videos/6.mp4")
+cap = cv2.VideoCapture("./Videos/7.mp4")
 # cv2.namedWindow("output", cv2.WINDOW_NORMAL)
 
 
-model = YOLO("../yolo-weights/yolov8l.pt")
+model = YOLO("./fine-tuned/final-bottle-model.pt")
 
-classNames = {
-    0: "person",
-    1: "bicycle",
-    2: "car",
-    3: "motorcycle",
-    4: "airplane",
-    5: "bus",
-    6: "train",
-    7: "truck",
-    8: "boat",
-    9: "traffic light",
-    10: "fire hydrant",
-    11: "stop sign",
-    12: "parking meter",
-    13: "bench",
-    14: "bird",
-    15: "cat",
-    16: "dog",
-    17: "horse",
-    18: "sheep",
-    19: "cow",
-    20: "elephant",
-    21: "bear",
-    22: "zebra",
-    23: "giraffe",
-    24: "backpack",
-    25: "umbrella",
-    26: "handbag",
-    27: "tie",
-    28: "suitcase",
-    29: "frisbee",
-    30: "skis",
-    31: "snowboard",
-    32: "sports ball",
-    33: "kite",
-    34: "baseball bat",
-    35: "baseball glove",
-    36: "skateboard",
-    37: "surfboard",
-    38: "tennis racket",
-    39: "bottle",
-    40: "wine glass",
-    41: "cup",
-    42: "fork",
-    43: "knife",
-    44: "spoon",
-    45: "bowl",
-    46: "banana",
-    47: "apple",
-    48: "sandwich",
-    49: "orange",
-    50: "broccoli",
-    51: "carrot",
-    52: "hot dog",
-    53: "pizza",
-    54: "donut",
-    55: "cake",
-    56: "chair",
-    57: "couch",
-    58: "potted plant",
-    59: "bed",
-    60: "dining table",
-    61: "toilet",
-    62: "tv",
-    63: "laptop",
-    64: "mouse",
-    65: "remote",
-    66: "keyboard",
-    67: "cell phone",
-    68: "microwave",
-    69: "oven",
-    70: "toaster",
-    71: "sink",
-    72: "refrigerator",
-    73: "book",
-    74: "clock",
-    75: "vase",
-    76: "scissors",
-    77: "teddy bear",
-    78: "hair drier",
-    79: "toothbrush",
-}
+classNames = {0: 'bottle'}
 
 while True:
     success, img = cap.read()
@@ -119,7 +38,9 @@ while True:
             conf = math.ceil((box.conf[0] * 100)) / 100
             # Class name
             cls = int(box.cls[0])
-            cvzone.putTextRect(img, f"{classNames[cls]} {conf}", (max(0, x1), max(35, y1)), scale=3, thickness=3)
-    imS = cv2.resize(img, (1280, 720))
-    cv2.imshow("Image", imS)
-    cv2.waitKey(0)
+            if conf > 0.8:
+                cvzone.putTextRect(img, f"{classNames[cls]} {conf}", (max(0, x1), max(35, y1)), scale=3, thickness=3)
+    # imS = cv2.resize(img, (1280, 720))
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
+
